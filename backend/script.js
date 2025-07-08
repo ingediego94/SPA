d8f4a38fe6399943d5e91be109cf71dd37e4a301
ingediego94 <inge.diego94@gmail.com>
2025-07-07 20:05:15 -0500

    Se agrega SPA

--- backend/script.js
+++ backend/script.js
@@ -1,29 +1,40 @@
 const routes = {
     "/": "./users.html",
+    "/users": "./users.html",
     "/newuser": "./newuser.html",
     "/about": "./about.html"
 };
 
 document.body.addEventListener("click", e =>{
     
-    if (e.target.matches("[data-link")){
+    if (e.target.matches("[data-link]")){
         e.preventDefault();
         
         // me muestra la ruta 
-        console.log(e.target.getAttribute("href"));
+        //console.log(e.target.getAttribute("href"));
         
-        //navigate()
+        navigate(e.target.getAttribute("href"))
+        //console.log(history);
+
     }
+
+    console.log(window);
+    
 });
 
 async function navigate(pathname){
     const route = routes[pathname];
 
-    console.log( route );
+    //console.log( route );
 
     const html = await fetch(route)
-        .then( res =>res.text() )
-        console.log(html)
+        .then( res => res.text() )
+       // console.log(html)
     document.getElementById("content").innerHTML = html
+    history.pushState({},"",pathname);
 
 } 
+
+window.addEventListener("popstate", ()=> 
+    navigate(location.pathname)
+);
